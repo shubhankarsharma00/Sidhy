defbee27b827c14abef57bb0e98e1277d28d88a3
AniketDogra <aniket.ddogra@gmail.com>
2019-03-09 10:26:12 +0530

    Added StaggeredGridView to the Dashboard

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,22 +1,94 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../style/theme.dart' as Theme;
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import '../widgets/subjectCard.dart';
 
-class DashBoard extends StatelessWidget {
+class DashBoard extends StatefulWidget {
+  @override
+  _DashBoardState createState() => _DashBoardState();
+}
+
+class _DashBoardState extends State<DashBoard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Dashboard"),
-      ),
-      body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to first screen when tapped!
-            Navigator.pushReplacementNamed(context,"/");
-          },
-          child: Text('LOGOUT!'),
+        elevation: 2.0,
+        backgroundColor: Theme.Colors.primaryColor,
+        title: Text(
+          "DASHBOARD",
+          style: TextStyle(
+              color: Colors.black, fontWeight: FontWeight.w700, fontSize: 30.0),
         ),
+        actions: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text('NITH',
+                    style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.w700,
+                        fontSize: 14.0)),
+                Icon(Icons.arrow_drop_down, color: Colors.black54)
+              ],
+            ),
+          )
+        ],
       ),
+      body: StaggeredGridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 12.0,
+          mainAxisSpacing: 12.0,
+          padding: EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
+          children: <Widget>[
+            _buildTile(
+              Padding(
+                padding: EdgeInsets.all(24.0),
+                child: SubjectCard(),
+              ),
+              onTap: () => {print("TESTING")}
+            ),
+            _buildTile(
+              Padding(
+                padding: EdgeInsets.all(24.0),
+                child: SubjectCard(),
+              ),
+              onTap: () => {print("TESTING")}
+            ),
+            _buildTile(
+              Padding(
+                padding: EdgeInsets.all(24.0),
+                child: SubjectCard(),
+              ),
+              onTap: () => {print("TESTING")}
+            ),
+           
+
+          ],
+          staggeredTiles: [
+            StaggeredTile.extent(2, 110.0),
+            StaggeredTile.extent(1, 180.0),
+            StaggeredTile.extent(1, 180.0),
+          ]),
     );
+  }
+
+  Widget _buildTile(Widget child, {Function() onTap}) {
+    return Material(
+        elevation: 14.0,
+        borderRadius: BorderRadius.circular(12.0),
+        shadowColor: Color(0x802196F3),
+        child: InkWell(
+            // Do onTap() if it isn't null, otherwise do print()
+            onTap: onTap != null
+                ? () => onTap()
+                : () {
+                    print('Not set yet');
+                  },
+            child: child));
   }
 }
