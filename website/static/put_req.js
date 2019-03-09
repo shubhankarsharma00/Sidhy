55dd5d1fc8b34748c051c70e162ee87a9a8e5471
Cupcake Rainbow <offendasmanyasucan@gmail.com>
2019-03-09 23:03:34 +0530

    Added Reset

---
+++ website/static/put_req.js
@@ -0,0 +1,12 @@
+   // $.ajax({
+   //    type: "PUT",
+   //    contentType: "application/json",
+   //    url: 'sidhy-33818.firebaseio.com/.json',
+   //    data: {"confidence":{"1-25":0,"25-50":0,"50-75":0,"75-100":0},"question":[],"votes":{"A":0,"B":0,"C":0,"D":0}},
+   //    dataType: "json"
+   // });
+
+
+$('#reset').click(function() {
+    alert("Hello");
+});
